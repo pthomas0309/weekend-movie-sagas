cb1ac40a3b6e7965f6482a9a7f6144f06cee2b40
pthomas0309 <preston.thomas355@gmail.com>
2021-06-20 23:14:13 -0500

    set inputs to be controlled by the properties of newMovie useState

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,15 +1,43 @@
+// bring in useState
+import { useState } from 'react';
+
 function MovieForm() {
+
+    // create newMovie variable with the useState function
+    const [newMovie, setNewMovie] = useState({
+        title: '',
+        poster: '',
+        description: '',
+        genres: ''
+    });
+
+    // function to update the newMovie
+    // object properties based on inputs
+    const updateMovie = (event) => {
+
+        // prevent page load
+        event.preventDefault();
+
+        // update movie object properties for post
+        setNewMovie({
+            ...newMovie,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    console.log(newMovie);
     return (
         <form>
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-            <select>
+            <input onChange={ (event) => {updateMovie(event)}} type="text" value={newMovie.title} name="title" required/><br/>
+            <input onChange={ (event) => {updateMovie(event)}} type="text" value={newMovie.poster} name="poster" required/><br/>
+            <input onChange={ (event) => {updateMovie(event)}} type="text" value={newMovie.description} name="description" required/><br/>
+            <select onChange={ (event) => {updateMovie(event)}} name="genres" value={newMovie.genres} required>
+                <option>Choose Genre</option>
                 <option>Placeholder</option>
             </select>
             <input type="submit" />
         </form>
     )
-}
+};
 
 export default MovieForm;
